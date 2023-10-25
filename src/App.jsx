c328be87f5313306ccca7a45164ab8975db89c3d
Arfiano Jordhy <arfianoj@gmail.com>
2023-10-25 22:06:59 +0700

    basic components, props, and state

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Menu from "./components/Menu";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello FSW 2</h1>
+      <Menu props1="this is props 1" props2="this is props 2" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./Menu.css";
+
+function Menu(props) {
+  let [kelas, setClass] = useState(false);
+  const clickHandler = () => {
+    setClass(!false);
+  };
+  return (
+    <div className="menu">
+      <h1>{kelas ? "FSW 2 Hore" : "FSW 2 Hore Hore"}</h1>
+      <button onClick={clickHandler}>Ganti Judul</button>
+      <ul>
+        <li>
+          <a href="#">menu 1</a>{" "}
+        </li>
+        <li>
+          <a href="#">menu 2</a>{" "}
+        </li>
+        <li>
+          <a href="#">menu 3</a>{" "}
+        </li>
+        <li>
+          <a href="#">menu 4</a>{" "}
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
